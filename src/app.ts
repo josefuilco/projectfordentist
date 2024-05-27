c9bb509ca200e518f50c0c499999a97825be2c56
Jose Fuilco <josefuilco@gmail.com>
2024-05-27 10:00:54 -0500

    Habilitando el cors para cualquier dominio

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     // origin: ["https://projectfordentist.com", "http://192.168.194.2:5173"],
+    origin: true,
     credentials: true,
   })
 );
